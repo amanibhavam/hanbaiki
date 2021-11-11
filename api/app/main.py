0e37969a09f12a4d2e5134040bd8e81f1c61afe0
Amanibhavam <iam@defn.sh>
2021-11-10 20:17:08 -0800

    run with hypercorn, debug friendly

--- api/app/main.py
+++ api/app/main.py
@@ -2,6 +2,10 @@ from typing import Optional
 
 from fastapi import FastAPI
 
+import asyncio
+from hypercorn.config import Config
+from hypercorn.asyncio import serve
+
 app = FastAPI()
 
 
@@ -12,4 +16,8 @@ def read_root():
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = "what's this?"):
+    breakpoint()
     return {"item_id": item_id, "q": q}
+
+
+asyncio.run(serve(app, Config()))
